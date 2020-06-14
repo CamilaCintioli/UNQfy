9039e23e79504963d147da78698dcd646561c4d4
Camila Cintioli <camila.cintioli@gmail.com>
2020-06-14 19:37:06 -0300

    Added unqfy error handling in artist router

--- api/artists.js
+++ api/artists.js
@@ -26,33 +26,48 @@ artistRouter.route('/')
     }
   )
   .get((req, res) => {
-    //validar que si no es name, es un bad request
     const artists = searchArtists(res.locals.unqfy, req.query.name);
     res.status(200).send({ status: 200, code: 'OK', artists });
   });
 
 artistRouter.route('/:id')
-  .get((req, res) => {
+  .get((req, res, next) => {
     const artistId = +req.params.id;
-    const artist = getArtistById(res.locals.unqfy, artistId);
-    res.status(200).send({ code: 200, stats: 'OK', artist });
+    try{
+      const artist = getArtistById(res.locals.unqfy, artistId);
+      res.status(200).send({ code: 200, stats: 'OK', artist });
+    }
+    catch(error){
+      next(unqfyError(error));
+    }
   })
   .put(createValidationMiddleware(yup.object({
     name: yup.string().trim().required('El nombre es requerido'),
     country: yup.string().trim().required('El pais es requerido'),
-  })),(req, res) => {
+  })),
+  (req, res, next) => {
     const artistId = +req.params.id;
     const artistData = {
       name: req.body.name,
       country: req.body.country,
     };
-    const artist = updateArtist(res.locals.unqfy, artistId, artistData);
-    res.status(200).send({ code: 200, status: 'OK', artist });
+    try{
+      const artist = updateArtist(res.locals.unqfy, artistId, artistData);
+      res.status(200).send({ code: 200, status: 'OK', artist });
+    }
+    catch(error){
+      next(unqfyError(error));
+    }
   })
-  .delete((req, res) => {
+  .delete((req, res, next) => {
     const artistId = +req.params.id;
-    deleteArtist(res.locals.unqfy, artistId);
-    res.status(204).send({ status: 204, code: 'NO CONTENT' });
+    try{
+      deleteArtist(res.locals.unqfy, artistId);
+      res.status(204).send({ status: 204, code: 'NO CONTENT' });
+    }
+    catch(error){
+      next(unqfyError(error));
+    }
   });
 
 module.exports = artistRouter;
