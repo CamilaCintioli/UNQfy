7baec3c63de2290a5f0a9199790f008dc117a465
Camila Cintioli <camila.cintioli@gmail.com>
2020-06-14 00:09:28 -0300

    Added validation to albums router

--- api/albums.js
+++ api/albums.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const yup = require('yup');
 const albumRouter = express.Router();
 const {addAlbum,searchAlbums,getAlbumById,updateAlbum,deleteAlbum} = require('../model/services/albumsService');
+const { createValidationMiddleware } = require('./validation');
 
 albumRouter.route('/')
-  .post((req,res) => {
+  .post(createValidationMiddleware(yup.object({
+    artistId:yup.number().strict(true).required('El id del artista es requerido'),
+    title:yup.string().trim().required('El titulo del album es requerido'),
+    year:yup.number().strict(true).required('El año del album es requerido'),
+  })),(req,res) => {
     const albumData = {title:req.body.title,year:req.body.year};
-    const album = addAlbum(res.locals.unqfy,+req.body.artistId,albumData);
+    const album = addAlbum(res.locals.unqfy,req.body.artistId,albumData);
     res.status(201).send({status:201,code:'CREATED',album});
   })
   .get((req,res) => {
@@ -19,7 +25,10 @@ albumRouter.route('/:id')
     const album = getAlbumById(res.locals.unqfy,albumId);
     res.status(200).send({code:200,stats:'OK',album});
   })
-  .put((req,res) => {
+  .put(createValidationMiddleware(yup.object({
+    title:yup.string().trim().required('El titulo del album es requerido'),
+    year:yup.number().strict(true).required('El año del album es requerido'),
+  })),(req,res) => {
     const albumId = +req.params.id;
     const albumData = {
       title:req.body.title,
