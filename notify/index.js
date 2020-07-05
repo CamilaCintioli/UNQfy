23dc782e5797aa5d173f55ecd308ea6af752008a
Camila Cintioli <camila.cintioli@gmail.com>
2020-07-05 14:52:14 -0300

    Added notify index

---
+++ notify/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { notifyMiddleware } = require('./middlewares/notify');
+const { verifyArtistIdMiddleware } = require('./middlewares/verifyArtistIdMiddleware');
+
+const app = express();
+const apiRouter = express.Router();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/api', notifyMiddleware, apiRouter);
+
+apiRouter.post('/subscribe',
+  verifyArtistIdMiddleware(),
+  (req, res, next) => {
+    res.locals.notificator.subscribe(req.body.artistId,req.body.email)
+    res.status(200).send('subscripto');
+  });
+
+app.listen(5000, () => console.log('Notify escuchando en puerto 5000'));
