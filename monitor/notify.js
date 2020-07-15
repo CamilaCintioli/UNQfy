bd35aa95aa23cfcc38dc435058a59f069454299d
gradaglio <gradaglio@hotmail.com>
2020-07-15 16:49:51 -0300

    Fix loggy y notify endpoints

--- monitor/notify.js
+++ monitor/notify.js
@@ -1,14 +1,29 @@
+const rp = require('request-promise')
 
 function notifyServiceFailure(service){
-  sendNotification(` -- ${service} ha dejado de funcionar`);
+  sendNotification(new Date().getHours() + ':' + new Date().getMinutes() + ` -- ${service} ha dejado de funcionar`);
 }
   
 function notifyServiceStartedWorking(service){
-  sendNotification(` -- ${service} ha vuelto a la normalidad`);
+  sendNotification(new Date().getHours() + ':' + new Date().getMinutes() + ` -- ${service} ha vuelto a la normalidad`);
 }
   
 function sendNotification(message){
-  console.log(message);
+  /*const options = {
+    url: 'https://hooks.slack.com/services/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',
+    body: {
+      text: message,
+    },
+    json: true,
+  };
+  rp.post(options).then(response => {
+    console.log('cool');
+  }).catch(err => {
+    console.log('algo malio sal');
+  });*/
+  console.log(message)
 }
+
+
 
 module.exports = { notifyServiceFailure, notifyServiceStartedWorking };
