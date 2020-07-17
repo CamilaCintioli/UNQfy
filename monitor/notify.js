37fa28909e748055809336a9d2e911c33843de06
Camila Cintioli <camila.cintioli@gmail.com>
2020-07-17 14:56:07 -0300

    Changed services.json

--- monitor/notify.js
+++ monitor/notify.js
@@ -1,4 +1,4 @@
-const rp = require('request-promise')
+const rp = require("request-promise");
 
 function notifyServiceFailure(service){
   sendNotification(new Date().getHours() + ':' + new Date().getMinutes() + ` -- ${service} ha dejado de funcionar`);
@@ -9,7 +9,8 @@ function notifyServiceStartedWorking(service){
 }
   
 function sendNotification(message){
-  /*const options = {
+  const options = {
+    method:'POST',  
     url: 'https://hooks.slack.com/services/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',
     body: {
       text: message,
@@ -19,9 +20,8 @@ function sendNotification(message){
   rp.post(options).then(response => {
     console.log('cool');
   }).catch(err => {
-    console.log('algo malio sal');
-  });*/
-  console.log(message)
+    console.log('algo malio sal', err.error, err.statusCode);
+  });
 }
 
 
